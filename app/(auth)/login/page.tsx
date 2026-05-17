afce8bb9c139fa166ef057814abe74b1eba88786
Prachi2519 <prachi639220@gmail.com>
2026-05-17 16:46:45 +0530

    auth setup

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import LoginUI from "@/module/auth/components/login-ui";
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginUI></LoginUI>
+    </div>
+  );
+};
---
+++ module/auth/components/login-ui.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const LoginUI = () => {
+  return <div>loginui</div>;
+};
+
+export default LoginUI;
